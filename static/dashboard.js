706bbcfd0c840646e1a74e5a6ad3e7d856138ef4
TokiLoshi <103384402+TokiLoshi@users.noreply.github.com>
2023-08-01 13:24:49 -0700

    Updated Dashboard to show questions

--- static/dashboard.js
+++ static/dashboard.js
@@ -4,12 +4,21 @@ fetch('https://sheetdb.io/api/v1/78uve0w4nrmgo')
   .then(response => response.json())
   .then(data => {
     console.log(data)
-    let container = document.querySelector('#knowledge')
+    let container = document.querySelector('.knowledgeTitle')
+    
     data.forEach(row => {
+      let stringData = JSON.stringify(row["question"])
       let rowElement = document.createElement('p')
-      rowElement.innerText = JSON.stringify(row);
-      container.append(rowElement)
+      rowElement.textContent = stringData;
+      container.appendChild(rowElement)
+      let button = document.createElement('button')
+      button.setAttribute('class', 'btn btn-primary text-center mb-2')
+      button.setAttribute('id', stringData.split(' ').join(''))
+      button.innerText = "tryItOut"
+      container.appendChild(button)
+      button.addEventListener('onclick', alert(`Question: ${button.id}`))
     })
   })
   .catch(error => console.log('Error', error))
+
  
