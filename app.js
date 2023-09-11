81c3a0422ac59ecd24d3ffb7f9c8e25a147962cf
TokiLoshi <103384402+TokiLoshi@users.noreply.github.com>
2023-09-10 20:00:30 -0700

    Updating port

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ let sessionConfig = {
   resave: false,
   saveUninitialized: true, 
   cookie: {
-    // httpOnly: true,
+    httpOnly: true,
     expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
     maxAge: 1000 * 60 * 60 * 24 * 7,
   }
