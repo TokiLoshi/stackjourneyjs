7d22d2824157b107fcf07da5b6b9cbdba73c5731
TokiLoshi <103384402+TokiLoshi@users.noreply.github.com>
2023-09-11 11:39:51 -0700

    Reorganizing directory structure

---
+++ login.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const loginRouter = express.Router()
+const flash = require('express-flash')
+
+loginRouter.get('/', (req, res) => {
+  console.log("Getting login")
+  res.render("login", {messages : req.flash()})
+})
+
+console.log("HI")
+
+loginRouter.route('/:id')
+.get((req, res) => {
+  console.log(req.user)
+  res.send(`Get user with ID: ${req.params.id}`)
+})
+.put((req, res) => {
+  res.send(`Update user with id ${req.params.id}`)
+})
+.delete((req, res) => {
+  res.send(`Delete user with id: ${res.params.id}`)
+})
+ 
+const users = [
+  {name: "Admin"},
+  {name: "Bee"}
+]
+
+loginRouter.param("id", (req, res, next, id) => {
+  console.log(id)
+  req.user = users[id]
+  next()
+})
+
+
+
+module.exports = loginRouter
