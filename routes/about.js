f1d0f7505b8e08d564bb1178e859658f8b8419ad
TokiLoshi <103384402+TokiLoshi@users.noreply.github.com>
2023-09-11 11:39:24 -0700

    Trying to fix routing

--- routes/about.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const aboutRouter = express.Router()
-
-aboutRouter.get('/', (req, res) => {
-  console.log("Getting about")
-  res.render("about")
-})
-
-module.exports = aboutRouter
